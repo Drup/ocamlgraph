a5bf4550fda9369d1a31c1d078d0be8b0a9705d4
Drup <drupyog@zoho.com>
2014-05-22 02:20:06 +0200

    Functorized Hashtbl and bugfix.

--- src/cutset.ml
+++ src/cutset.ml
@@ -1,54 +1,66 @@
 module Make (G : Sig.G) = struct
 
+  (* We use the hash and equal functions provided by the user. *)
+  module H = Hashtbl.Make (G.V)
+
+  let find_default d htbl x =
+    try H.find htbl x
+    with Not_found -> false
+
   let min_cutset gr first_node =
-    let n_labels : (G.V.t,_) Hashtbl.t = Hashtbl.create (G.nb_vertex gr) in
-    let l_labels = Hashtbl.create (G.nb_vertex gr) in
-    let already_processed = Hashtbl.create (G.nb_vertex gr) in
-    let on_the_stack = Hashtbl.create (G.nb_vertex gr) in
+    let n_labels = H.create (G.nb_vertex gr) in
+    let l_labels = H.create (G.nb_vertex gr) in
+
+    let already_processed = H.create (G.nb_vertex gr) in
+    let is_already_processed x = find_default false already_processed x in
+
+    let on_the_stack = H.create (G.nb_vertex gr) in
+    let is_on_the_stack x = find_default false on_the_stack x in
+
     let cut_set = ref [] in
     let counter = ref 1 in
 
     let rec step2 top rest_of_stack =
-      assert (not (Hashtbl.find already_processed top));
-      assert (not (Hashtbl.find on_the_stack top));
-      Hashtbl.add on_the_stack top true;
-      Hashtbl.add n_labels top !counter;
+      assert (not (is_already_processed top));
+      assert (not (is_on_the_stack top));
+      H.add on_the_stack top true;
+      H.add n_labels top !counter;
       counter := !counter + 1;
-      Hashtbl.add l_labels top 0;
-      Hashtbl.add already_processed top true;
+      H.add l_labels top 0;
+      H.add already_processed top true;
       step3 (G.succ_e gr top) top rest_of_stack
 
     and step3 edges top rest_of_stack =
       match edges with
         | edge::other_edges ->
             let v = G.E.dst edge in
-            if not (Hashtbl.find already_processed v)
+            if not (is_already_processed v)
             (* step 4 *)
             then step2 v ((top,edges)::rest_of_stack)
             (* step 5 *)
             else
               begin
                 let x =
-                  if Hashtbl.find on_the_stack v
-                  then Hashtbl.find n_labels v
-                  else Hashtbl.find l_labels v
+                  if is_on_the_stack v
+                  then H.find n_labels v
+                  else H.find l_labels v
                 in
-                Hashtbl.add l_labels top
-                  (max (Hashtbl.find l_labels top) x) ;
+                H.add l_labels top
+                  (max (H.find l_labels top) x) ;
                 step3 other_edges top rest_of_stack
               end
 
         | [] ->
             begin
               (* step 7 *)
-              if Hashtbl.find l_labels top = Hashtbl.find n_labels top
+              if H.find l_labels top = H.find n_labels top
               then begin
                 cut_set := top::!cut_set ;
-                Hashtbl.add l_labels top 0 ;
+                H.add l_labels top 0 ;
               end ;
 
               (* check added between algorithms C and D *)
-              if Hashtbl.find l_labels top > Hashtbl.find n_labels top
+              if H.find l_labels top > H.find n_labels top
               then None (* FAILURE (graph not reducible) *)
               else
 
@@ -57,11 +69,11 @@ module Make (G : Sig.G) = struct
                   | [] -> Some !cut_set (* SUCCESS *)
                   | (new_top, new_edges)::new_tail ->
                       begin
-                        Hashtbl.add on_the_stack top false;
-                        Hashtbl.add l_labels new_top
+                        H.add on_the_stack top false;
+                        H.add l_labels new_top
                           (max
-                             (Hashtbl.find l_labels top)
-                             (Hashtbl.find l_labels new_top)) ;
+                             (H.find l_labels top)
+                             (H.find l_labels new_top)) ;
                         step3 new_edges new_top new_tail
                       end)
             end in
